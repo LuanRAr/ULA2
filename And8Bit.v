8b756970a40b819cc26b89bfcfda454b18d3d66e
Luan.RAr <134662776+LuanRAr@users.noreply.github.com>
2025-10-02 10:53:43 -0300

    Create And8Bit.v
    And de 8 bits criado

---
+++ And8Bit.v
@@ -0,0 +1,15 @@
+module And8Bit(A, B, S);
+input [7:0] A, B;
+output [7:0] S;
+
+and and0(S[0], A[0], B[0]);
+and and1(S[1], A[1], B[1]);
+and and2(S[2], A[2], B[2]);
+and and3(S[3], A[3], B[3]);
+
+and and4(S[4], A[4], B[4]);
+and and5(S[5], A[3], B[3]);
+and and6(S[6], A[3], B[3]);
+and and7(S[7], A[3], B[3]);
+
+endmodule
