fb7a14b32d02197a0076f19c4f396fbe6adf7cae
Luan.RAr <luanlra002@gmail.com>
2025-10-06 23:47:56 -0300

    negando A

--- Not8bit.v
+++ Not8bit.v
@@ -1,24 +1,15 @@
-module Not8bit(A, B, S);
-	input [7:0] A, B;
-	output [15:0] S;
+module Not8bit(
+    input  [7:0] A, 
+    output [7:0] S 
+);
 
-	not not0(S[0], A[0]);
-	not not1(S[1], A[1]);
-	not not2(S[2], A[2]);
-	not not3(S[3], A[3]);
-	not not4(S[4], A[4]);
-	not not5(S[5], A[5]);
-	not not6(S[6], A[6]);
-	not not7(S[7], A[7]);
-	
-	not not0b(S[8], B[0]);
-	not not1b(S[9], B[1]);
-	not not2b(S[10], B[2]);
-	not not3b(S[11], B[3]);
-	not not4b(S[12], B[4]);
-	not not5b(S[13], B[5]);
-	not not6b(S[14], B[6]);
-	not not7b(S[15], B[7]);
-  
+    not U0_inv (S[0], A[0]);
+    not U1_inv (S[1], A[1]);
+    not U2_inv (S[2], A[2]); 
+    not U3_inv (S[3], A[3]); 
+    not U4_inv (S[4], A[4]); 
+    not U5_inv (S[5], A[5]); 
+    not U6_inv (S[6], A[6]); 
+    not U7_inv (S[7], A[7]); 
   
 endmodule
